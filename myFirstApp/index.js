d7bd134316e09d400d169328e6ea9d70104f8732
Zerdov <juleswillig08@gmail.com>
2025-10-03 11:24:56 +0200

    myFirstApp

---
+++ myFirstApp/index.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+
+function getQuestions() {
+  return fs.readFileSync("./z_questions.txt", "utf8").split(/\r?\n/);
+}
+
+function ask(questions, index) {
+  if (index >= questions.length) {
+    console.log("Thank you");
+    process.exit();
+  }
+
+  process.stdout.write(questions[index] + " ");
+
+  process.stdin.once("data", (answer) => {
+    fs.appendFileSync("./z_answers.txt", `${questions[index]} -> ${answer.toString().trim()}\n`);
+    ask(questions, index + 1);
+  });
+}
+
+function main() {
+  const questions = getQuestions();
+  console.log(questions.filter((question) => question.trim() !== ""))
+  if (questions.every((question) => question.trim() === "")) {
+    return;
+  }
+  return ask(questions, 0);
+}
+
+main();
